bd19469dbb607f313c0f3cd542a473411f78305d
Kevin Li <kevin.li195@hotmail.com>
2015-12-04 22:59:05 -0500

    Start on q2, convolve_fast.c with variable decls

--- convolve_fast.c
+++ convolve_fast.c
@@ -1,3 +1,12 @@
 #include <fast_filter.h>
 
+void main(int argc, char *argv[]) {
+    FILE *input_bmp, *output_bmp;
+    int filter_width;
+    input_bmp = fopen(argv[1], "r");
+    output_bmp = fopen(argv[2], "w");
+    filter_width = atoi(argv[3]);
+    filter_width_sq = (int)pow((double) filter_width, 2);
+
+}
 
